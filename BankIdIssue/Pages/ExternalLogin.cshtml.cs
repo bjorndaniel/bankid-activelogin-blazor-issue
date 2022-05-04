5e6b573041f28e2062b18c4ed735ad87210759fc
Daniel Sandberg <bjorndaniel@gmail.com>
2022-05-04 07:05:35 +0200

    Working before changes to startup

---
+++ BankIdIssue/Pages/ExternalLogin.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankId.Pages;
+public class ExternalLoginModel :PageModel
+{
+    private readonly IHttpContextAccessor _accessor;
+
+    public ExternalLoginModel(IHttpContextAccessor accessor)
+    {
+        _accessor = accessor;
+    }
+
+	public IActionResult OnGet(string provider)
+    {
+        var request = _accessor.HttpContext!.Request;
+        var baseURL = $"https://{request.Host}";
+        var props = new AuthenticationProperties
+        {
+            RedirectUri = $"{baseURL}/externallogincallback",
+            Items =
+    {
+        {"returnUrl", "~/"},
+        {"scheme", provider}
+    }
+        };
+
+        return Challenge(props, provider);
+    }
+}
+
---
+++ BankIdIssue/Pages/ExternalLoginCallback.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace BankId.Pages;
+public class ExternalLoginCallbackModel : PageModel
+{
+    private readonly IHttpContextAccessor _accessor;
+    private readonly ILogger<ExternalLoginCallbackModel> _logger;
+
+    public ExternalLoginCallbackModel(IHttpContextAccessor accessor, ILogger<ExternalLoginCallbackModel> logger)
+    {
+        _accessor = accessor;
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> OnGet()
+    {
+        var result = await _accessor.HttpContext!.AuthenticateAsync();
+        var request = _accessor.HttpContext!.Request;
+        var _baseURL = $"{request.Scheme}://{request.Host}/landingpage";
+        if (result?.Succeeded != true)
+        {
+            throw new Exception("External authentication error");
+        }
+        if (_accessor.HttpContext.User?.Identity is ClaimsIdentity identity)
+        {
+            var extra = identity.Claims.FirstOrDefault(_ => _.Type == "ExtraClaim")?.Value;
+            Console.WriteLine($"ExtraClaim: {extra}");
+        }
+        return Redirect(_baseURL);
+    }
+}
